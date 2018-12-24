c2e5196036576860fe467105e97a20950641c1ac
TonyJR <qintao@shiqichuban.com>
2018-12-24 16:38:44 +0800

    修改端口

--- server.py
+++ server.py
@@ -53,7 +53,7 @@ class ImageServer(object):
         tornado.ioloop.IOLoop.current().start()
 
 if __name__ == "__main__":
-    server_port = "8080"
+    server_port = "80"
     server = ImageServer(server_port)
     print "begin server"
     server.process()
