e16a040d18b9ee78231a8a0b652c6274edeba314
TonyJR <qintao@shiqichuban.com>
2018-12-25 11:34:31 +0800

    设置跨域访问

--- server.py
+++ server.py
@@ -16,6 +16,11 @@ sys.setdefaultencoding('utf8')
 
 
 class Handler(tornado.web.RequestHandler):
+    def set_default_header(self):
+        # 后面的*可以换成ip地址，意为允许访问的地址
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Headers', 'x-requested-with')
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
     
     def get(self):
         image_url = self.get_argument("image_url", default="")
