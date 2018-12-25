c3788cda75a2f1dde2dbb897b28463b2c83106de
TonyJR <qintao@shiqichuban.com>
2018-12-25 11:55:53 +0800

    设置跨域访问

--- server.py
+++ server.py
@@ -17,6 +17,12 @@ sys.setdefaultencoding('utf8')
 
 class Handler(tornado.web.RequestHandler):
     
+    def set_default_headers(self):
+        print（"setting headers!!!"）
+        self.set_header("Access-Control-Allow-Origin", "*") # 这个地方可以写域名
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    
     def get(self):
         
         self.set_header('Access-Control-Allow-Origin', '*')
