56013751d55d74cdce1432170a6dd469e2301d39
TonyJR <qintao@shiqichuban.com>
2020-01-07 15:15:48 +0800

    修复gif无法加载的问题

--- image.py
+++ image.py
@@ -18,10 +18,10 @@ def save_img(img_url):
         firstLine = str(response.readline())
         nowTime = time.time()#生成当前的时间
         randomNum = random.randint(0,100000)#生成随机数n,其中0<=n<=100
-        if firstLine.find('PNG') >= 0:
-            filename = "/tmp/com.shiqichuban.image-sharpening/"+str(nowTime) + "_" + str(randomNum)+".png"
-        else :
+        if firstLine.find('JPG') >= 0:
             filename = "/tmp/com.shiqichuban.image-sharpening/"+str(nowTime) + "_" + str(randomNum)+".jpg"
+        else :
+            filename = "/tmp/com.shiqichuban.image-sharpening/"+str(nowTime) + "_" + str(randomNum)+".png"
         
         
         with open(filename, 'wb') as f:
